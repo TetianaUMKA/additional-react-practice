85dab4318185827c2856329592fd2bc24408a1d9
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-10-27 11:46:53 +0200

    Create Aside

---
+++ src/components/Aside/Aside.jsx
@@ -0,0 +1,12 @@
+export default function Aside() {
+  return (
+    <aside>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam eum
+        voluptates eos. Perspiciatis nobis expedita distinctio ipsam quis
+        incidunt est rem non, ut quaerat autem praesentium quo vel perferendis?
+        Nihil!
+      </p>
+    </aside>
+  );
+}
