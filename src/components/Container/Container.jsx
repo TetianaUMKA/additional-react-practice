e1277af0a6b0a0cd731c2ff9b2a46a82bfdb1ad1
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-11-23 20:34:25 +0200

    Update style settings

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,5 @@
-import style from "./Container.module.css";
+import css from "./Container.module.css";
 
 export default function Container({ children }) {
-  return <div className={style.container}>{children}</div>;
+  return <div className={css.container}>{children}</div>;
 }
